b9af97c965ce22f96219f9e5dfc135da3dfbf0a4
Melvyn de Kort <melvyn@mdekort.nl>
2025-09-16 17:19:16 +0200

    Remove actual secret, pull from remote state instead

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "cloudflare_api_token" {
   name  = "/mc/cloudflare/api_token"
   type  = "SecureString"
-  value = local.secrets.cloudflare.api_token
+  value = data.terraform_remote_state.tf_cloudflare.outputs.api_token_minecraft
 }
